bb65c5daf52f2e8224477ff1512d0733890fa06c
Henry Vega <hp.vega@hotmail.com>
2026-08-11 22:11:55 -0500

    wip

---
+++ sesion-1/ejemplos/operadores_matematicos.py
@@ -0,0 +1,12 @@
+# operadores matematicos
+
+# aritméticos
+suma = 5 + 8
+resta = 4 - 3
+multiplicacion = 6.8 * 4
+division = 4 / 2
+
+# operadores matematicos directos
+numero = 5
+numero += 5 # esto representa ---> numero = numero + 5
+print(numero)
