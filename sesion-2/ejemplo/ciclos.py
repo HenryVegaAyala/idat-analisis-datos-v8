c9af1bf642f9db8d5712d444c3b7478e54bc3f3a
Henry Vega <hp.vega@hotmail.com>
2026-08-13 20:15:45 -0500

    wip

---
+++ sesion-2/ejemplo/ciclos.py
@@ -0,0 +1,7 @@
+# Ciclo con python
+
+# For -> Recorrer cada elemento de la lista
+frutas = ["fresa", "limon", "piña", "naranja", "platano"]
+
+for fruta in frutas:
+    print(f"Nombre de la fruta {fruta}")
