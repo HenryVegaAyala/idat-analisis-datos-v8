fc0bef0513674ba934731be321a9954a79ac093e
Henry Vega <hp.vega@hotmail.com>
2026-08-13 22:14:38 -0500

    wip

--- sesion-2/calcular_igv.py
+++ sesion-2/calcular_igv.py
@@ -5,7 +5,7 @@ def convertir_a_soles(monto_dolares: float | int, tipo_de_cambio: float = 3.80):
 monto_dolares = float(input("Introduce el monto de dolares: "))
 tipo_de_cambio = input("Introduce el tipo de cambio por defecto es [3.80]: ")
 
-if tipo_de_cambio == "":
+if tipo_de_cambio == "": # comparativo de lo que recibo por consola y lo comparo con un cadena vacia que es doble comilla
     resultado_tipo_de_cambio = convertir_a_soles(monto_dolares)
 else:
     tipo_de_cambio = float(tipo_de_cambio)  # formatear el tipo de dato de cadena a numero
