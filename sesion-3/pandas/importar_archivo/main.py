58d225e1bc5bece160c6cb1bdb27fbfc0824e2cc
Henry Vega <hp.vega@hotmail.com>
2026-08-18 22:05:05 -0500

    wip

--- sesion-3/pandas/importar_archivo/main.py
+++ sesion-3/pandas/importar_archivo/main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# Lectura de un archivo csv
 df = pd.read_csv("data_enero.csv")
+print(df)
 
+# Lectura de un archivo xlsx
+df = pd.read_excel("ejemplo_excel.xlsx")
 print(df)
