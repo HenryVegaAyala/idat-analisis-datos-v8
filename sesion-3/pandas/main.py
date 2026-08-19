feb6263e1c663e951e5a23c6262b7cfc3e2f5e42
Henry Vega <hp.vega@hotmail.com>
2026-08-18 21:49:56 -0500

    wip

--- sesion-3/pandas/main.py
+++ sesion-3/pandas/main.py
@@ -7,4 +7,12 @@ data = {
 
 df = pd.DataFrame(data)
 
+print(f"Ejemplo de un dataframe:")
 print(df)
+
+data = [20, 60, 50, 90, 60]
+
+serie = pd.Series(data)
+
+print(f"Ejemplo de un serie:")
+print(serie)
