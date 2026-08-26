c285f027859509c8bec9d1236114c6fe623130ef
Henry Vega <hp.vega@hotmail.com>
2026-08-25 21:25:08 -0500

    wip

---
+++ sesion-5/pandas/limpieza_datos/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+data = pd.read_csv("facturacion_datos.csv")
+
+# Fillna -> Reemplazar por valores
+data["id_cliente"] = data["id_cliente"].fillna("Desconocido")
+
+print(data)
+print("-" * 50)
+
+# Dropna se encarga de eliminar todos los valores null
+data_sin_nan = data.dropna()
+
+print(data_sin_nan)
+print("-" * 50)
+
+# Drop_duplicates se encarga de eliminar valores duplicados
+
+data_corregida = data.drop_duplicates()
+print(data_corregida)
