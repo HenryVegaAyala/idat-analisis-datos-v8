8ecd9215a152aa102057e1826274a6836f97be27
Henry Vega <hp.vega@hotmail.com>
2026-08-27 20:11:30 -0500

    wip

---
+++ sesion-6/graficos/overlay/main.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from openpyxl.chart import label, marker
+
+# datos del mes
+mes = ["Enero", "Febrero", "Marzo", "Abril"]
+
+# gastos mensualkes de cada servicio en soles
+luz = [40, 50, 56, 60]
+agua = [25, 30, 31, 35]
+
+# Crear grafico de gastos de luz
+plt.plot(
+    mes,
+    luz,
+    label="Servicio de luz",
+    marker="o",
+    linewidth=3,
+    color="blue"
+)
+
+# Crear grafico de gastos de agua
+plt.plot(
+    mes,
+    agua,
+    label="Servicio de agua",
+    marker="s",
+    linewidth=3,
+    color="red"
+)
+
+plt.title("Gastos de la casa")
+plt.xlabel("Mes")
+plt.ylabel("Gastos S/.")
+
+plt.show()
